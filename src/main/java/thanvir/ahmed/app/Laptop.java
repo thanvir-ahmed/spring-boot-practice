56f23153f8896976896b66ec74817aec5a2cce53
Thanvir Ahmed <thanvir.ahmed@bjitgroup.com>
2025-08-08 17:28:59 +0600

    first commit

---
+++ src/main/java/thanvir/ahmed/app/Laptop.java
@@ -0,0 +1,10 @@
+package thanvir.ahmed.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile() {
+        System.out.println("Compiling...");
+    }
+}
---
+++ src/main/java/thanvir/ahmed/app/SpringBootDemoApplication.java
@@ -0,0 +1,18 @@
+package thanvir.ahmed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
+
+//        context.getBean(Laptop.class).compile();
+    }
+}
